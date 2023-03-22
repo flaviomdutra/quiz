c8c852d4a494c957afb55215b595a67870331fa4
Flávio Dutra <flaviomdutra@outlook.com>
2023-03-21 23:07:19 -0300

    feat: add sound and haptics

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { Alert, ScrollView, Text, View } from "react-native";
-
+import { Alert, BackHandler, ScrollView, Text, View } from "react-native";
+import { Audio } from "expo-av";
 import { useNavigation, useRoute } from "@react-navigation/native";
-
+import * as Haptics from "expo-haptics";
 import { styles } from "./styles";
 
 import { QUIZ } from "../../data/quiz";
@@ -57,6 +57,17 @@ export function Quiz() {
   const route = useRoute();
   const { id } = route.params as Params;
 
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require("../../assets/correct.mp3")
+      : require("../../assets/wrong.mp3");
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true });
+
+    await sound.setPositionAsync(0);
+    await sound.playAsync();
+  }
+
   function handleSkipConfirm() {
     Alert.alert("Pular", "Deseja realmente pular a questão?", [
       { text: "Sim", onPress: () => handleNextQuestion() },
@@ -93,10 +104,13 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
-      setStatusReply(1);
       setPoints((prevState) => prevState + 1);
+
+      await playSound(true);
+      setStatusReply(1);
       handleNextQuestion();
     } else {
+      await playSound(false);
       setStatusReply(2);
       shakeAnimation();
     }
@@ -120,7 +134,8 @@ export function Quiz() {
     return true;
   }
 
-  function shakeAnimation() {
+  async function shakeAnimation() {
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
     shake.value = withSequence(
       withTiming(3, {
         duration: 400,
@@ -214,6 +229,15 @@ export function Quiz() {
     const quizSelected = QUIZ.filter((item) => item.id === id)[0];
     setQuiz(quizSelected);
     setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      handleStop
+    );
+
+    return () => backHandler.remove();
   }, []);
 
   if (isLoading) {
